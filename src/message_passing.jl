cd03c2b9f1c96ca57060367d000d6d9ce14b8e9d
honzabim <honzabim@gmail.com>
2019-04-16 16:15:08 +0200

    commented out the message passing test case so it compiles

--- src/message_passing.jl
+++ src/message_passing.jl
@@ -17,17 +17,19 @@ function message_passing(xx, bags, instance_model, aggregation, vertex_model, n:
     x
 end
 message_passing(ds::BagNode, instance_model, aggregation, vertex_model, n::Int) = message_passing(ds.data.data, ds.bags, instance_model, aggregation, vertex_model, n)
-#test case
-bags = ScatteredBags([setdiff(neighbors(g, i), i) for i in vertices(g)])
-ds = BagNode(vertex_properties, bags)
-k = 4
-rounds = 3
-instance_model = Dense(k, k, relu)
-aggregation = SegmentedMeanMax(k)
-vertex_model = Dense(2k, k)
-message_passing(ds, instance_model, aggregation, vertex_model, rounds) # put this to test
-a = Mill.Aggregation((MessagePassing(instance_model, aggregation, vertex_model, rounds),))
-m = BagModel(BagModel(identity, a, identity), SegmentedMeanMax(k), Chain(Dense(2k, k, relu), Dense(k,2))) 
-dss = BagNode(ds, [1:nobs(ds)])
-m(dss)
-m(cat(dss,dss)).data
+
+
+# #test case
+# bags = ScatteredBags([setdiff(neighbors(g, i), i) for i in vertices(g)])
+# ds = BagNode(vertex_properties, bags)
+# k = 4
+# rounds = 3
+# instance_model = Dense(k, k, relu)
+# aggregation = SegmentedMeanMax(k)
+# vertex_model = Dense(2k, k)
+# message_passing(ds, instance_model, aggregation, vertex_model, rounds) # put this to test
+# a = Mill.Aggregation((MessagePassing(instance_model, aggregation, vertex_model, rounds),))
+# m = BagModel(BagModel(identity, a, identity), SegmentedMeanMax(k), Chain(Dense(2k, k, relu), Dense(k,2))) 
+# dss = BagNode(ds, [1:nobs(ds)])
+# m(dss)
+# m(cat(dss,dss)).data
